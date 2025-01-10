c89509e8239642844c2031b9d87291eec53ab665
BusratSabiha <bsse1311@iit.du.ac.bd>
2025-01-10 21:42:45 +0600

    testing deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Sabiha!');
+  res.send('Hello World!');
 });
 
 if (require.main === module) {
